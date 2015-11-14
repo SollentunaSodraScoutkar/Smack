b997208f9a6f37493a6e65b2c7c74b20c0b9e24e
Thomas Bindzau <thomas.bindzau@gmail.com>
2015-11-14 11:31:41 +0100

    Added Attendance table

--- DBChanges.sql
+++ DBChanges.sql
@@ -1,1 +1,71 @@
-﻿
+﻿--Create version table if it does not exist
+IF OBJECT_ID (N'Databaseversion', N'U') IS NULL 
+BEGIN
+	CREATE TABLE Databaseversion
+	(
+		Id INT IDENTITY(1,1) PRIMARY KEY,
+		Version INT NOT NULL,
+		Timestamp DATETIME2 DEFAULT GETDATE()
+	)
+	INSERT INTO Databaseversion (Version) VALUES (0)
+END
+
+DECLARE @currentVersion INT
+SELECT @currentVersion = MAX(Version) FROM DatabaseVersion
+
+DECLARE @changeVersion INT
+
+PRINT 'Version when starting script: ' 
+PRINT @currentVersion
+
+--TEMPLATE:
+/*
+-------------------------------------------------
+SELECT @changeVersion = [replace with version number]
+IF @currentVersion < @changeVersion 
+BEGIN
+	/*
+	INSERT SQL HERE!
+	*/
+	INSERT INTO DatabaseVersion (Version) VALUES (@changeVersion)
+END
+--------------------------------------------------	
+*/
+	
+-------------------------------------------------
+SELECT @changeVersion = 1
+IF @currentVersion < @changeVersion 
+BEGIN
+	CREATE TABLE Attendance (
+		[intAttendanceId] [int] IDENTITY (1,1) PRIMARY KEY NOT NULL,
+		[varName] varchar(100) NULL,
+		[dtmAttendanceDate] datetime NOT NULL,
+		[intDivisionId] [int] NULL,
+		[blnConfirmed] [bit] NULL
+		)
+
+	CREATE TABLE MemberAttendance (
+		[intAttendanceId] [int] NOT NULL,
+		[intMemberId] [int] NOT NULL,
+		PRIMARY KEY CLUSTERED 
+(
+	[intAttendanceId] ASC,
+	[intMemberId] ASC))
+
+	ALTER TABLE Member ADD CONSTRAINT PK_MemberId PRIMARY KEY CLUSTERED (intMemberID)
+
+	ALTER TABLE [dbo].[MemberAttendance]  WITH CHECK ADD  CONSTRAINT [FK_Attendance_MemberAttendance] FOREIGN KEY([intAttendanceId])
+	REFERENCES [dbo].[Attendance] ([intAttendanceId])
+	ALTER TABLE [dbo].[MemberAttendance]  WITH CHECK ADD  CONSTRAINT [FK_Member_MemberAttendance] FOREIGN KEY([intMemberId])
+	REFERENCES [dbo].[Member] ([intMemberID])
+
+	INSERT INTO DatabaseVersion (Version) VALUES (@changeVersion)
+END
+--------------------------------------------------	
+-------------------------------------------------
+
+SELECT @currentVersion = MAX(Version) FROM DatabaseVersion
+
+PRINT 'Version after completing script: ' 
+PRINT @currentVersion
+
